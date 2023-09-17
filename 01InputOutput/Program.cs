1e701b8d29142e40cb9ea76068639fb7b01f21cf
AnthonyCoppens1 <anthonycoppens95@gmail.com>
2023-09-17 15:02:18 +0200

    NewFiles

---
+++ 01InputOutput/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace InputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine("Hello {0}!", name);
+
+            /*{0} is a placeholder, you can use as many placeholders as you'd like.
+             * E.g. Console.WriteLine("Hello {0}{1}{2}!", x,y,z);
+             * with x y z being variables you've declared earlier*/
+
+
+            string x = "Jan";
+            string y = "Tom";
+            string z = "Flip";
+            Console.WriteLine("Hello {0} {1} {2}!", x, y, z);
+
+            //options for comments
+            /* if you want to write on multiple lines
+
+            this is possible*/
+
+
+        }
+    }
+}
