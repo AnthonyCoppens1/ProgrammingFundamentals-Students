e65e23995cd38f49852f29e013eece9c9dae0364
AnthonyCoppens1 <anthonycoppens95@gmail.com>
2024-03-15 12:43:58 +0100

    Week10

--- Week10/10OO-Hospital-DSPS2/obj/Debug/net6.0/10OO-Hospital-DSPS2.AssemblyInfo.cs
+++ Week10/10OO-Hospital-DSPS2/obj/Debug/net6.0/10OO-Hospital-DSPS2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("10OO-Hospital-DSPS2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d37e3ece38b20c1d8726d5bcf859eecb9905dbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("10OO-Hospital-DSPS2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("10OO-Hospital-DSPS2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
